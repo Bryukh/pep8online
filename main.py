a087a7871be30517d76d0939a851e57a8efe99a7
Valentin Bryukhanov <bvv.mag@gmail.com>
2012-09-09 17:30:34 +1200

    Empty template for root page

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #-*- encoding: utf8 -*-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def paste_page():
+    """
+    Main page with form for paste code
+    """
+    return render_template("paste_page.html")
 
 #For development
 if __name__ == '__main__':
